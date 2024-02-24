2cc52e1b10c95760505890a18c128815d0ede796
BhukuDon <53051051+BhukuDon@users.noreply.github.com>
2024-02-24 21:29:22 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E Clock Timer
+ 
---
+++ builder/builder.py
@@ -0,0 +1,15 @@
+import PyInstaller.__main__
+import os,shutil
+PyInstaller.__main__.run([
+    'main.py',
+    '--onefile',
+    '--windowed',
+    "--clean",
+    "--name=E Clock",
+    "--icon=assets\icon.ico",
+    "--distpath=",
+
+])
+
+os.remove("E Clock.spec")
+shutil.rmtree("build")
